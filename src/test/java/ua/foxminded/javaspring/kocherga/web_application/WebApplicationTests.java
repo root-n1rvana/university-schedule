639da5b9afb9882749eef2017b34c8b6f15e1006
N1rvana <moon.hqm@gmail.com>
2023-06-03 00:00:56 +0200

    initial commit

---
+++ src/test/java/ua/foxminded/javaspring/kocherga/web_application/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.foxminded.javaspring.kocherga.web_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
