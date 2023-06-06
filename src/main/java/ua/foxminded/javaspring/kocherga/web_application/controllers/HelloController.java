87fed908e8e8a49e0757d02e78749819e1400c3d
N1rvana <moon.hqm@gmail.com>
2023-06-06 22:56:47 +0200

    first working version

---
+++ src/main/java/ua/foxminded/javaspring/kocherga/web_application/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package ua.foxminded.javaspring.kocherga.web_application.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
+}
